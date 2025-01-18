310ded179749c0a732c5e7ab7e5d7512a1cb0fc5
keigratz <113896749+DidacDV@users.noreply.github.com>
2025-01-18 18:37:54 +0100

    added basic export2pdf functionallity, but styles dont work correctly when exporting -> TODO

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,19 @@
 import './styles/header.css'
+import html2pdf from 'html2pdf.js'; // Correct import
+
 const Header = () => {
+
+    const handleExport = () => {
+        const el = document.querySelector('.previewContainer');
+        html2pdf().from(el).save("resume.pdf");
+    }
+
     return (
         <header className='headerContent'>
             <h1>CV Generator</h1>
             <nav>
                 <a rel="noopener noreferrer" target="_blank" href = "https:github.com" >GitHub</a>
-                <a>By Dac</a>
+                <a onClick={handleExport}>Export to PDF</a>
             </nav>
         </header>
     );
